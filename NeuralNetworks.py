d9c5a70b7521043ca105ef31e11b6fc2a8b4d3af
Nicolas Almiron <57641897+Nalmiron23@users.noreply.github.com>
2024-03-20 18:16:35 +0200

    Implement back-propagation algorithm
    -Define a function named backprop to compute gradients for a neural network.
    -Extract necessary parameters and cached values from provided dictionaries (parameters and cache).
    +Compute gradients for the weights and biases of each layer using backpropagation equations.
    -Store computed gradients in a dictionary named grads.
    -Return the gradients dictionary.

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -60,3 +60,26 @@ def compute_cost(A2, Y):
     
     return cost
 
+# Backprop
+def backprop(parameters, cache, X, Y):
+
+    m = X.shape[1]
+    
+    # Get parameters from dict in cache
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+    A1 = cache['A1']
+    A2 = cache['A2']
+    
+    # Compute gradients
+    dZ2 = A2 - Y
+    dW2 = (1/m) * np.dot(dZ2,A1.T)
+    db2 = (1/m) * np.sum(dZ2, axis=1,keepdims=True)
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+    dW1 = (1/m) * np.dot(dZ1,X.T)
+    db1 = (1/m) *  np.sum(dZ1, axis=1,keepdims=True)
+    
+    # Store grads in dict
+    grads = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
+    
+    return grads
